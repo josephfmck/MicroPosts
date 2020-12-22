b9354a51693a12d14bad145491eb5cee91184d7e
Joe <josephfmck@gmail.com>
2020-12-22 16:35:57 -0600

    copy beginner examples to 113-traverseyJS, clear app.js of beginner examples and delete modules, now ready for actual app

--- src/app.js
+++ src/app.js
@@ -1,26 +1,2 @@
-//CommonJS Module syntax NOT ES6/ES2015
-//const person = require('./mymodule1'); //file uses ./   
-  //require node modules without ./ require('express')
 
-
-//ES2015/ES6 syntax
-//import { person, sayHello } from './mymodule2';
-
-//console.log(person.name);
-
-//console.log(sayHello());
-
-
-//import all *, as some custom named obj var mod, from module
-//import * as mod from './mymodule2';
-
-// console.log(typeof mod);
-// console.log(mod.person.name);
-
-// console.log(mod.sayHello());
-
-//  import using default, dont need {}
-import greeting from './mymodule2';
-
-console.log(greeting);
 
--- src/mymodule1.js
+++
@@ -1,8 +0,0 @@
-//module can be anything
-
-//common js syntax, NOT ES6/ES2015
-//export makes access available to other files
-module.exports = {
-    name: 'Joe',
-    email: 'test@test.com'
-}
