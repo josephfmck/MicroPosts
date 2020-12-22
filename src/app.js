cb01b941f4bbbfc66a92ece469bb3261b022fbcd
Joe <josephfmck@gmail.com>
2020-12-22 15:56:33 -0600

    create module2 for importing with ES6 syntax

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-//common js way NOT ES6/ES2015
-const person = require('./mymodule1'); //file uses ./   
+//CommonJS Module syntax NOT ES6/ES2015
+//const person = require('./mymodule1'); //file uses ./   
   //require node modules without ./ require('express')
 
+
+//ES2015/ES6 syntax
+import { person, sayHello } from './mymodule2';
+
+
 console.log(person.name);
+
+console.log(sayHello());
+
---
+++ src/mymodule2.js
@@ -0,0 +1,9 @@
+//ES6 syntax
+export const person = {
+    name: 'joe',
+    age: 25
+}
+
+export function sayHello() {
+    return `Hello ${person.name}`;
+}
