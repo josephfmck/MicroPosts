191febc73958849dd5f1d17e15e5aacca7353807
Joe <josephfmck@gmail.com>
2020-12-28 14:28:58 -0600

    118 Add Posts and Show Alert, create submitPost() thats exec with submit click event, grab and set data from inputs to data obj, create Post in data by exec http.post() that handles post request with the data obj, exec getPosts() to display posts again

--- src/app.js
+++ src/app.js
@@ -4,10 +4,36 @@ import { ui } from "./ui";
 //  Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+//  Listen for submitting post 
+document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+//  Get Posts and display
 function getPosts() {
     //get returns promise, so need .then
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
         .catch(err => console.log(err));
-};
+}
+
+//  Submit post to posts
+function submitPost() {
+    //  get form data
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+
+    // create data obj with props = to values grabbed
+    const data = {
+        title: title,
+        body: body
+    }
+
+    //  Create Post
+        //  Gets added to db.json
+        //post request gives us a promise so .then
+    http.post('http://localhost:3000/posts', data)
+        .then(data => {
+            //  Call to display with new post
+            getPosts();
+        })
+        .catch(err => console.log(err));
+}
