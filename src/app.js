c1ab0e00f9ba95bf5ccd4970e8eaf9075a62c38e
Joe <josephfmck@gmail.com>
2020-12-28 16:42:18 -0600

    120 Post Edit State and Update, create enableEdistState() event func, check for e.target with .edit, grab post data and set to obj

--- src/app.js
+++ src/app.js
@@ -9,7 +9,11 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 
 //  Listen for delete post
     //event delegation so grab parent #posts
-document.querySelector('#posts').addEventListener('click', deletePost)
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+//  Listen for Edit State
+    //event delegation
+document.querySelector('#posts').addEventListener('click', enableEditState)
 
 //  Get Posts and display
 function getPosts() {
@@ -68,6 +72,30 @@ function deletePost(e) {
                 .catch(err => console.log(err));
         }
     }
+
+    e.preventDefault();
+}
+
+//  Enable Edit State
+function enableEditState(e) {
+    if(e.target.parentElement.classList.contains('edit')) {
+        //  Grab id from dataset
+        const id = e.target.parentElement.dataset.id;
+        //  Grab body text
+        const body = e.target.parentElement.previousElementSibling.textContent;
+        //  Grab title text
+        const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+
+        //  Set data obj with post data grabbed
+        const data = {
+            id: id,
+            title: title,
+            body: body
+        }
+
+        
+    }
+
 
     e.preventDefault();
 }
